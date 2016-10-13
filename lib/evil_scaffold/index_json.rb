ba5abe9afedcf52561d29dd7426c8a87aa51a3c4
conanite <conan@conandalton.net>
2016-10-13 17:16:24 +0200

    index_json: refactor to allow grouping of options in order to generate <optgroup> elements with #select2

--- lib/evil_scaffold/index_json.rb
+++ lib/evil_scaffold/index_json.rb
@@ -11,15 +11,28 @@ module EvilScaffold
           { id: item.id, text: item.name }
         end
 
+        def index_json_optgroups items
+          nil
+        end
+
+        def index_json_per_page
+          20
+        end
+
         def index_json
-          per_page = isset(:per_page) ? params[:per_page].to_i : 20
-          page     = isset(:page)     ? params[:page].to_i     : 1
+          per_page                = isset(:per_page) ? params[:per_page].to_i : index_json_per_page
+          page                    = isset(:page)     ? params[:page].to_i     : 1
           @#{config.models_name}  = @#{config.models_name}.paginate page: page, per_page: per_page
-          json = {
-            results: @#{config.models_name}.map { |item| #{config.model_name}_to_json item },
-            pagination: { more: (page * per_page) < @#{config.models_name}.total_entries }
-          }
-          render json: json
+          optgroups               = index_json_optgroups @#{config.models_name}
+          results                 = if optgroups.nil?
+                                       @#{config.models_name}.map { |item| #{config.model_name}_to_json item }
+                                    else
+                                      optgroups.map { |grouping, items|
+                                        { text: grouping, children: items.map { |item| #{item_to_json} item } }
+                                      }
+                                    end
+          pagination              = { more: (page * per_page) < @#{config.models_name}.total_entries }
+          render json: { results: results, pagination: pagination }
         end
 INDEX_JSON
     end
