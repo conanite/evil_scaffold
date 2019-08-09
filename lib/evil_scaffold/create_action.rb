76eb585268694f733592d6a36ca74f42833684c1
conanite <conan@conandalton.net>
2019-08-09 08:52:32 +0200

    create: provide a bit more control over whether and what gets created by providing #authorise_creation and #sanitise_for_create overrides

--- lib/evil_scaffold/create_action.rb
+++ lib/evil_scaffold/create_action.rb
@@ -5,16 +5,29 @@ module EvilScaffold
     def self.install config
       return unless config.for? :create
       config.install <<ACTION, __FILE__, __LINE__
+        # by default permits all attrs, override if you need to be more selective about your attributes
+        def sanitise_for_create attrs
+          attrs.permit!
+        end
+
+        # override this to respond appropriately in case of a validation error
         def create_validation_failure thing
           raise(ActiveRecord::RecordInvalid.new(thing))
         end
 
+        # override this and raise an error if thing cannot be created for any reason other than validation error
+        def authorise_creation thing
+          thing
+        end
+
+        # this is called after #create for any xhr request
         def ajax_after_create
           render "ajax_create", layout: false
         end
 
+        # this is the #create action
         def create
-          @#{config.model_name} = #{config.model_class_name}.new params[:#{config.model_name}].permit!
+          @#{config.model_name} = authorise_creation #{config.model_class_name}.new sanitise_for_create params[:#{config.model_name}]
           if @#{config.model_name}.save
             if request.xhr?
               ajax_after_create
